af61adcf1c5e50297b407276204e8fc051b73a5c
Ilya Zakharov <mackzwellz@yandex.ru>
2019-08-22 18:21:51 +0400

    Task 4 initial commit

---
+++ src/com/sperasoft/java_fundamentals/mandatory/task_4/CountSymbolOccurrence.java
@@ -0,0 +1,42 @@
+package com.sperasoft.java_fundamentals.mandatory.task_4;
+
+import java.util.Scanner;
+
+public class CountSymbolOccurrence {
+
+    public static void main(String[] args) {
+
+        String stringToSearchIn = "_1_234561_2345_16asdfaaddssa\n" +
+                "#~`!`~ ノಠ益ಠノ彡┻━┻ Angry Table-flIp\t" +
+                "`~!@#$%^&*(){}[]/\\<>?.,\"\"'':;_-+=№|\n" +
+                "√Ω≈ç∫∞§…¬å∑± a \n" +
+                "\uD83D\uDC69\uD83C\uDFFF 1238 ＝═⚌﹦ https://аррӏе.com\n";
+
+        System.out.println(stringToSearchIn);
+        System.out.println("Input a symbol to search for in above text:");
+
+        /* Parse inputs:
+        /* https://www.javatpoint.com/java-string-to-char */
+        Scanner scan = new Scanner(System.in);
+        char symbolToSearchFor = scan.next().charAt(0);
+        scan.close();
+
+
+        findAndCountSymbolInString(stringToSearchIn, symbolToSearchFor);
+    }
+
+    private static void findAndCountSymbolInString(String stringToSearchIn,
+                                                   char symbolToSearchFor) {
+        int numberOfOccurrences = 0;
+
+        /* https://way2java.com/string-and-stringbuffer/string-finding-matching-characters/ */
+
+        for (int i = 0; i < stringToSearchIn.length(); i++) {
+            if (stringToSearchIn.charAt(i) == symbolToSearchFor) {
+                numberOfOccurrences++;
+            }
+        }
+            System.out.println("Symbol \"" + symbolToSearchFor + "\" is found " + numberOfOccurrences + " times in above text.");
+
+    }
+}
